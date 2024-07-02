2ede04b8964ce6c744b80b3c73d80351f4101725
Luiz Lima <lblima@limalinux.LIMAHOUSE>
2024-07-02 11:16:45 -0300

    Primeira versão do projeto RentCar.

---
+++ RentCar.js
@@ -0,0 +1,189 @@
+class Carro{
+    constructor(placa,ano,cor,modelo,km,diaria,obs){
+        this.placa = placa;
+        this.ano = ano;
+        this.cor = cor;
+        this.modelo = modelo;
+        this.km = km; 
+        this.diaria = diaria;
+        this.obs  = obs;
+    }
+}
+class CEsportivo extends Carro{
+    constructor(placa,ano,cor,modelo,km,diaria,obs,tempKmHora,melhorias){
+        super(placa,ano,cor,modelo,km,diaria,obs);
+        this.tempKmHora = tempKmHora; 
+        this.melhorias = melhorias;
+    }
+}
+class CUtilitario extends Carro{
+    constructor(placa,ano,cor,modelo,km,diaria,obs,codigo,qtddPassageiros,tmhBagageiro,kmLitro){
+        super(placa,ano,cor,modelo,km,diaria,obs);
+        this.codigo =codigo;
+        this.qtddPassageiros = qtddPassageiros;
+        this.tmhBagageiro = tmhBagageiro;
+        this.kmLitro = kmLitro;
+    }
+}
+class Reserva{
+    constructor(codigo,cli,carro,situacao,dtInicio,dtFim){
+        this.codigo = codigo;
+        this.cli = cli;
+        this.carro = carro;
+        this.situacao = situacao;
+        this.dtInicio = dtInicio;
+        this.dtFim = dtFim;
+
+    }
+    setStatus(situacao){
+        this.situacao = situacao;
+    }
+}
+class Pessoa{
+    constructor(nome,cpf,idade,endereco,telCntt){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.endereco = endereco;
+        this.telCntt = telCntt;
+
+    }
+}
+class Funcionario extends Pessoa{
+    constructor(nome,cpf,idade, endereco,telCntt,dtContrata,salario,qtddAlugueis, sttAtivoInativo){
+        super(nome,cpf,idade,endereco,telCntt);
+        this.dtContrata = dtContrata;
+        this.salario = salario;
+        this.qtddAlugueis =  qtddAlugueis;
+        this.sttAtivoInativo = sttAtivoInativo;
+    }
+    setSituacao(sttAtivoInativo){
+        this.sttAtivoInativo = sttAtivoInativo;
+
+    }
+}
+class Cliente extends Pessoa{
+    constructor(nome,cpf,idade, endereco, telCntt,dtNasc,cartMotorista,fotoCartMotor,cartMotorVencimento,email){
+        super(nome,cpf,idade,endereco,telCntt);
+        this.dtNasc = dtNasc;
+        this.cartMotorista = cartMotorista;
+        this.fotoCartMotor = fotoCartMotor;
+        this.cartMotorVencimento =  cartMotorVencimento;
+        this.email = email;
+    }
+    getCPF(){
+        return this.cpf;
+    }
+    getNome(){
+        return this.nome;
+    }
+}
+class Promocao{
+    constructor(titulo,descricao,validade){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.validade = validade;
+    }
+    
+}
+
+class Gerenciamento{
+    constructor(){
+        this.clientes = [];
+        this.funcionarios = [];
+        this.reservas = [];
+        this.carros = [];
+    }
+    addCli(cli){
+        this.clientes.push(cli);
+    }
+    getCli(cpf){
+        return this.clientes.filter(cli => {
+            if(cli.getCPF() == cpf){
+                return cli;
+            }
+        })[0];
+        
+    }
+    getListarCli(){
+        return this.clientes;
+    }
+    addCli(nome,cpf,idade, endereco, telCntt,dtNasc,cartMotorista,fotoCartMotor,cartMotorVencimento,email){
+        let novoCli = new Cliente(nome,cpf,idade, endereco, telCntt,dtNasc,cartMotorista,fotoCartMotor,cartMotorVencimento,email);
+        this.clientes.push(novoCli);
+        return novoCli;
+    }
+    
+    addFuncionario(nome,cpf,idade, endereco,telCntt,dtContrata,salario,qtddAlugueis, sttAtivoInativo){
+        let novoFuncionario = new Funcionario(nome,cpf,idade, endereco,telCntt,dtContrata,salario,qtddAlugueis, sttAtivoInativo);
+        this.funcionarios.push(novoFuncionario);
+        return novoFuncionario;
+    }
+    
+    addCarro(tipoCarro){
+        switch(tipoCarro){
+            case "Esportivo":
+                let novoEsportivo = new CEsportivo("JUN10001",2020,'Vermelho','Mod1',0,50.00,'Carro Esportivo');
+                this.carros.push(novoEsportivo);
+                return novoEsportivo;
+            case 'Utilitário':
+                let novoUtilitario = new CUtilitario('JUN20001',2019,'Preto','Mod2',0,45.00,'Carro Utilitário');
+                this.carros.push(novoUtilitario);
+                return novoUtilitario;
+            default:
+                console.error("Erro ao tentar criar tipo de carro informado!")
+        }
+    }
+    
+    addReserva(cli,carro,situacao,dtInicio,dtFim){
+        if(this.validaReseserva(cli)){
+            let codigo = this.reservas.length + 1;
+            let novaReserva = new Reserva(cli,codigo,situacao,dtInicio,dtFim);
+            this.reservas.push(novaReserva);
+            return novaReserva;
+        }else{
+            console.log('O Cliente ja possui uma reserva ativa!')
+        }
+        console.log(this.reservas);
+    }
+    
+    validaReseserva(cli){
+        let reserva = this.reservas.find(reserva => {
+            console.log(reserva);
+            reserva.cli.cpf == cli.cpf;
+        });
+        console.log(reserva);
+    }
+    
+    getCarros(){
+        return this.carros;
+    }
+    
+    getClientes(){
+        return this.clientes;
+    }
+    
+    getReservas(){
+        return this.reservas;
+    }
+    
+    getDesativarReserva(reserva){
+    
+    }
+}
+
+
+
+//var rent1 = new Gerenciamento();
+//var joao = new Cliente("João","111.222.333-34",28,"Rua 1 220 B CasaNova",'319999252525','01-01-16',111111,'foto','01-01-50','joao@email');
+//console.log(joao);
+//rent1.addCli(joao);
+//console.log(rent1.getListarCli());
+
+
+
+var rent2 = new Gerenciamento();
+rent2.addCarro('Esportivo');
+var fusca = rent2.addCarro('Utilitário');
+var joao = rent2.addCli("João","111.222.333-34",28,"Rua 1 220 B CasaNova",'319999252525','01-01-16',111111,'foto','01-01-50','joao@email');
+rent2.addReserva(joao,fusca,"Ativo","26/06/24","30/06/24");
---
+++ RentCar.md
@@ -0,0 +1,14 @@
+02/07/2024    
+    Desafio final da primeira etapa do curso:
+    A Localiza, uma conhecida rede brasileira especializada em aluguel de carros, solicitou o seu serviço como desenvolvedor de software para implementar o novo sistema de gerenciamento de toda a parte de locação e devolução de carros, assim como também o cadastro de carros, clientes e funcionários. Ao finalizar a reunião de levantamento de requisitos você identificou as seguintes necessidades:
+        Cada carro possui os seguintes registros: placa, ano, cor, modelo, quilometragem, valor diário, observação.
+        A localiza possui duas categorias de carros, os esportivos que possuem o tempo para chegar à 100 quilômetros por hora, lista de melhorias (você pode adicionar alguns casos genéricos) e os utilitários que possuem o atributo de quantidade de passageiros, tamanho do bagageiro, km por litro de gasolina.
+        Um carro está relacionado a uma reserva que possui um cliente associado, um código, uma flag de status, data início e uma data fim.
+        Uma reserva está sempre associada somente a um cliente e um cliente pode ter somente uma reserva ativa.
+        O funcionário possui o registro de nome, cpf, idade, endereço, data da contratação, salário, quantidade de aluguéis realizados e o status que pode ser ativado ou desativado e o telefone para contato.
+        Um cliente precisa possuir o nome, cpf, idade, data de nascimento, número da carteira de motorista, foto da carteira de motorista, ano de vencimento da carteira de motorista, endereço, telefone de contato e um e-mail.
+        Além disso, a Localiza deseja enviar promoções para o e-mail dos clientes cadastrados no sistema, essa promoção deve ter um título, uma descrição e uma data de validade.
+
+        Sua tarefa é analisar todos os requisitos acima, elaborar caso sinta a necessidade um diagrama UML do que será implementado e desenvolver o software seguindo asnecessidades descritas pelo cliente. Para te auxiliar nesse início do desenvolvimento estamos disponibilizando o diagrama abaixo que é um esboço do que deve ser criado (perceba que ele não atende todos os requisitos solicitados). Você está livre para modificar ele conforme o seu entendimento do problema proposto.
+
+        Esta é a primeira versão irei criar este mesmo projeto usando python.
